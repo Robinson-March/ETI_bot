ac0125f7708007e4af423d80d22aebc85aa0610a
ikaychina <amaechinaikechukwu6@gmail.com>
2025-03-28 20:55:55 +0100

    change to the start message

--- commands/start.ts
+++ commands/start.ts
@@ -3,11 +3,7 @@ import { optionsKeyboard, startKeyboard } from "../utils/keyboards";
 
 bot.command("start", (ctx) =>
   ctx.reply(
-    `Welcome to Elite Trading Institution!
-    
-    I am your friendly neighbourhood payment bot, you can explore our services below.
-
-If you need assistance please <a href="http://t.me/Elitestraderadmin">message/reply here</a> and my team will be happy to direct you.`,
+    `If you need assistance please click on contact us button and my team will be happy to direct you`,
     { parse_mode: "HTML", reply_markup: optionsKeyboard }
   )
 );
