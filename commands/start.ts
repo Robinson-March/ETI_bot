46b9e5e8bd5d127ef7af82491f1bfdeb8aca093f
ikaychina <amaechinaikechukwu6@gmail.com>
2025-03-28 21:15:06 +0100

    start up message updated

--- commands/start.ts
+++ commands/start.ts
@@ -3,7 +3,13 @@ import { optionsKeyboard, startKeyboard } from "../utils/keyboards";
 
 bot.command("start", (ctx) =>
   ctx.reply(
-    `If you need assistance please click on contact us button and my team will be happy to direct you`,
+    `
+    Welcome to <a href="https://elitetradinginstitution.com/">Elite Training Institution</a>.
+
+I am your friendly neighbourhood payment bot, you can explore our services below.
+
+
+    If you need assistance please click on contact us button and my team will be happy to direct you`,
     { parse_mode: "HTML", reply_markup: optionsKeyboard }
   )
 );
--- commands/whoWeAre.ts
+++ commands/whoWeAre.ts
@@ -1,6 +1,6 @@
 import { bot } from "../bot";
 
-const whoWeAreMessage = `<a href="https://elitetradinginstitution.com/">EliteTrainingInstitution</a> 🥇 VIP!\n\nWe are a professional trading group dedicated to helping traders succeed!`;
+const whoWeAreMessage = `<a href="https://elitetradinginstitution.com/">Elite Training Institution</a> 🥇 VIP!\n\nWe are a professional trading group dedicated to helping traders succeed!`;
 
 bot.command("who_we_are", (ctx) =>
   ctx.reply(whoWeAreMessage, { parse_mode: "HTML" })
