0ad111f7233b8006db9d6aa48df705494bc88cc2
ikaychina <amaechinaikechukwu6@gmail.com>
2025-03-28 21:16:34 +0100

    start up message updated

--- commands/start.ts
+++ commands/start.ts
@@ -9,7 +9,7 @@ bot.command("start", (ctx) =>
 I am your friendly neighbourhood payment bot, you can explore our services below.
 
 
-    If you need assistance please click on contact us button and my team will be happy to direct you`,
+If you need assistance please click on contact us button and my team will be happy to direct you`,
     { parse_mode: "HTML", reply_markup: optionsKeyboard }
   )
 );
